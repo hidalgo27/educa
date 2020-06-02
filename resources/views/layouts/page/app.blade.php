361a4ca82275cedb9470abbe4e252d1cfb6c780b
Hidalgo Mac <hidalgoponce@icloud.com>
2020-06-01 19:03:30 -0500

    Update

--- resources/views/layouts/page/app.blade.php
+++ resources/views/layouts/page/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Green Academy</title>
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
