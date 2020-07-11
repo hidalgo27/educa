6c5ca944a0cfba044e1227edba45d2dfbde34775
Hidalgo Mac <hidalgoponce@icloud.com>
2020-07-10 19:57:07 -0500

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -96231,8 +96231,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/freddy/Homestead/codigo/educa/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/freddy/Homestead/codigo/educa/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/hidalgoponce/web/sites/educa/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/hidalgoponce/web/sites/educa/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
